4503e56419a73135bcd3dfad2197e57ade3631f8
Rafael Infante <infantebruno41@gmail.com>
2024-04-29 09:43:52 -0600

    Refrescado

--- Laboratorios/laboratorio 02.R
+++ Laboratorios/laboratorio 02.R
@@ -5,8 +5,30 @@
 
 # Importar datos ----------------------------------------------------------
 
+# Utilizar funcion read.csv sirve para importar datos
 
+cr <- read.csv("Cedro.csv", header = TRUE) 
 
 # Revision de Datos -------------------------------------------------------
 
+mean(cr$diametro)
+mean(cr$altura)
+mean(cr$diametro);median(cr$diametro)
 
+sd(cr$diametro); sd(cr$altura)
+range(cr$diametro)
+
+fivenum(cr$diametro)
+
+
+# Representacion grafica --------------------------------------------------
+
+boxplot(cr$diametro)
+boxplot(cr$altura, col = "green",
+        ylim=c(10,28), ylab = "Altura (cm)",
+        main = "Cedro Rojo")
+
+hist(cr$altura)
+
+stem(cr$altura)
+ Se llama tallo y hojas se le el numero y a un lado cuantas veces se repite un valor 
